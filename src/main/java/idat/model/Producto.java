f547a202c9c66e7da94e45743f16e46d922b97bf
TeresaM83 <terezacerouno@gmail.com>
2022-09-27 20:23:32 -0500

    bd

--- src/main/java/idat/model/Producto.java
+++ src/main/java/idat/model/Producto.java
@@ -1,6 +1,17 @@
 package idat.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="productos")
 public class Producto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
--- src/main/java/idat/repository/ProductoRepository.java
+++ src/main/java/idat/repository/ProductoRepository.java
@@ -1,17 +1,11 @@
 package idat.repository;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import idat.model.Producto;
 
-public interface ProductoRepository {
-	void save(Producto p);
-	void actualizar(Producto p);
-	void eliminar(Integer codigo);
-	Producto buscar(Integer codigo);
-	Optional<Producto> buscarPrimero();
-	Producto buscarUltimo();
-	Optional<Producto> buscarAleatorio();
-	List<Producto> listar();
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+
 }
