4af2e24bdec96436979b6a4415e36226577004f7
TeresaMartinez <terezacerouno@gmail.com>
2022-11-08 20:59:27 -0500

    oauth

--- src/main/java/idat/security/ConfigSecurity.java
+++ src/main/java/idat/security/ConfigSecurity.java
@@ -16,6 +16,8 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -28,18 +30,7 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter{
 	@Autowired
 	private UserDetailService service;
 	
-	@Autowired
-	private TokenFilter filter;
-	
-	@Autowired
-	private EntryPoint point;
-	
-	@Override
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		// TODO Auto-generated method stub
-		return super.authenticationManagerBean();
-	}
-
+	@Bean
 	@Override
 	protected AuthenticationManager authenticationManager() throws Exception {
 		// TODO Auto-generated method stub
@@ -61,30 +52,6 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter{
 		arg0.userDetailsService(service).passwordEncoder(encrip());
 	}
 
-	@Override
-	protected void configure(HttpSecurity arg0) throws Exception {
-		/*arg0.authorizeRequests()
-		.antMatchers(HttpMethod.GET).permitAll()
-		.antMatchers("/producto/*").hasAnyRole("ADMIN")
-		.and()
-		.httpBasic()
-		.and()
-		.csrf().disable();*/
-		
-		arg0.authorizeRequests()
-		.antMatchers("/crearToken").permitAll()
-		.anyRequest()
-		.authenticated()
-		.and()
-		.exceptionHandling()
-		.authenticationEntryPoint(point)
-		.and()
-		.sessionManagement()
-		.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-		.and()
-		.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
-		.csrf().disable();
-	}
 	
 	@Bean
 	//Que este metodo lo puedo utilizar en cualquier parte
@@ -92,28 +59,10 @@ public class ConfigSecurity extends WebSecurityConfigurerAdapter{
 		return new BCryptPasswordEncoder();
 	}
 	
-/*	public UserDetailsService userdetail() {
-		
-		InMemoryUserDetailsManager manager= new InMemoryUserDetailsManager();
-		manager.createUser(
-					User.withUsername("profesor")
-					.password(encrip().encode("123"))
-					.roles("ADMIN")
-					.build()
-				);
-		return manager;
+	@Bean
+	public TokenStore tokenStore() {
+		return new InMemoryTokenStore();
 	}
 	
-	
-	@Bean
-	public SecurityFilterChain filter(HttpSecurity http)  throws Exception{
-		http.authorizeRequests()
-		.antMatchers("/producto/*").access("hasRole('ADMIN')")
-		.and()
-		.httpBasic()
-		.and()
-		.csrf().disable();
-		return http.build();
-	}*/
 
 }
